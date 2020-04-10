e2259947fc3deab7e19ec9cc6152993988fd1c72
daniel <daniel.khapun@gmail.com>
2020-04-10 15:51:04 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/smash.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/Code/OS/oshw1/Command.cpp" "/home/daniel/Code/OS/oshw1/cmake-build-debug/CMakeFiles/smash.dir/Command.cpp.o"
+  "/home/daniel/Code/OS/oshw1/PWDcommand.cpp" "/home/daniel/Code/OS/oshw1/cmake-build-debug/CMakeFiles/smash.dir/PWDcommand.cpp.o"
+  "/home/daniel/Code/OS/oshw1/State.cpp" "/home/daniel/Code/OS/oshw1/cmake-build-debug/CMakeFiles/smash.dir/State.cpp.o"
+  "/home/daniel/Code/OS/oshw1/Terminal.cpp" "/home/daniel/Code/OS/oshw1/cmake-build-debug/CMakeFiles/smash.dir/Terminal.cpp.o"
+  "/home/daniel/Code/OS/oshw1/smash.cpp" "/home/daniel/Code/OS/oshw1/cmake-build-debug/CMakeFiles/smash.dir/smash.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
