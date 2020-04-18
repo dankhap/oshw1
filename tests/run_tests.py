0c5fc98f81b2b8ad7cd7c8611c7e6e508f5c3ec6
daniel <daniel.khapun@gmail.com>
2020-04-18 18:47:37 +0300

    added support for signals

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,8 @@
-import subprocess
+from subprocess import Popen, check_output, PIPE
 import os
 from os import path
+from time import sleep
+
 
 def load_test_list():
     mypath = '.'
@@ -12,15 +14,24 @@ def load_test_list():
     return res
 
 def run_test(input, output_file):
-    ps = subprocess.Popen(('cat', input), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('../smash'), stdin=ps.stdout)
-    output = output.decode("utf-8")
+    with open(input, 'r') as input_file:
+        test_commands = input_file.readlines()
+    ps = Popen(('../smash'), stdout=PIPE, stdin=PIPE, text=True)
+    output_lines = []
+    for command in test_commands:
+        if command.startswith('@wait'):
+            sleep(int(command[5:]))
+            command = ''
+        elif command.startswith('@sig'):
+            ps.send_signal(int(command[4:]))
+            command = ''
+        output_lines.append(ps.communicate(command)[0])
     with open(output_file+'.o', 'w+') as f:
-        f.write(output)
+        f.writelines(output_lines)
 
     with open(output_file, 'r') as out:
-        out = out.read()
-    return out == output
+        out = out.readlines()
+    return out == output_lines
 
 if __name__ == "__main__":
     tests = load_test_list()
